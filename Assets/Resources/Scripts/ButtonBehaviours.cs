af5eb0be33a9590b6c908d6733b71b242bb113cc
Evan Funnell <evanfunnell@hotmail.com>
2018-04-12 14:25:44 -0600

    Added Pocket Inventory and mostly working wrist inventory

--- Assets/Resources/Scripts/ButtonBehaviours.cs
+++ Assets/Resources/Scripts/ButtonBehaviours.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonBehaviours : MonoBehaviour {
+    Inventory_Swipe inventory;
+
+    void Start() {
+        inventory = GetComponentInParent<Inventory_Swipe>();
+    }
     void OnTriggerEnter(Collider coll) {
         if ((gameObject.activeSelf) && (coll.gameObject.name.Contains("index3"))) {
             if (gameObject.name.Contains("Left")) {
-                Debug.Log("Going Left!");
+                if (inventory.currentDisplayedObj > 0) {
+                    inventory.currentDisplayedObj -= 1;
+                    inventory.currentObjChanged = true;
+                    Debug.Log(inventory.currentDisplayedObj);
+                }
             }
             if (gameObject.name.Contains("Right")) {
-                Debug.Log("Going Right");
+                if (inventory.currentDisplayedObj < (inventory.objects.Count - 1) ) {
+                    inventory.currentDisplayedObj += 1;
+                    inventory.currentObjChanged = true;
+                    Debug.Log(inventory.currentDisplayedObj);
+                }
             }
         }
     }
